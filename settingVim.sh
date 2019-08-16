54c33bc212f8ee640b42a56ddf131d1ca9a7c49d
cwho <cwho@nuvoton.com>
2019-08-16 16:07:41 +0800

    fixed issue

--- settingVim.sh
+++ settingVim.sh
@@ -9,18 +9,24 @@
 
 sudo apt install ctags cscope clang-format
 # NERDTree
-git clone https://github.com/scrooloose/nerdtreet ~/.vim/bundle/nerdtree
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
 mkdir tmptosetVim
 cd tmptosetVim 
 git clone https://github.com/tpope/vim-pathogen 
 cp vim-pathogen/autoload ~/.vim/ -r
+git clone https://github.com/kana/vim-operator-user
+cp vim-operator-user/autoload/operator ~/.vim -r
+cp vim-operator-user/doc/ ~/.vim -r
+
 git clone https://github.com/rhysd/vim-clang-format
 cp vim-clang-format/autoload/* ~/.vim/autoload/ -r
 cp vim-clang-format/doc/* ~/.vim/doc/
-cp vim-clang-format/plugin/* ~/.vim/plugin/
+cp vim-clang-format/plugin ~/.vim/ -r
+git clone https://github.com/kien/ctrlp.vim 
+cp ctrlp.vim ~/.vim/bundle/ -r
+git clone https://github.com/tacahiroy/ctrlp-funky 
+cp ctrlp-funky ~/.vim/bundle/ -r
+
 cd ..
-rm tmptosetVim -r
-git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/
-git clone https://github.com/tacahiroy/ctrlp-funky ~/.vim/bundle/
-
-
+sudo rm tmptosetVim -r
+cp .vimrc ~
